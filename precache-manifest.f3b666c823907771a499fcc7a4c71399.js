91ef5934a5ef35040b21b83f51336b3c61fd60f8
Nikhita Tithi <nikhita.tithi@gmail.com>
2020-08-12 02:34:02 +0530

    Updates

---
+++ precache-manifest.f3b666c823907771a499fcc7a4c71399.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02cf4ebafac20c778f364acff8be6c8f",
+    "url": "/shopingCartChallenge/index.html"
+  },
+  {
+    "revision": "0e8944d921f1aed9bef5",
+    "url": "/shopingCartChallenge/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5de1c43391dc9a5c764f",
+    "url": "/shopingCartChallenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e8944d921f1aed9bef5",
+    "url": "/shopingCartChallenge/static/js/2.5b429747.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/shopingCartChallenge/static/js/2.5b429747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de1c43391dc9a5c764f",
+    "url": "/shopingCartChallenge/static/js/main.59f0abfc.chunk.js"
+  },
+  {
+    "revision": "a8b7e2ddff45a6002010",
+    "url": "/shopingCartChallenge/static/js/runtime-main.1331f1b1.js"
+  }
+]);
